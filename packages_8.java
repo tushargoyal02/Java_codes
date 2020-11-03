92713268dff6d59eee5ee59b86f9db8d2e149dc6
Tushar Goyal <tushargoyal19tj@gmail.com>
2020-11-03 21:40:44 +0530

    packages example

---
+++ packages_8.java
@@ -0,0 +1,17 @@
+import java.util.Date;
+import java.util.*;
+class packages_8{
+    
+    
+
+public static void main(String[] args) {
+Date date;
+ArrayList list;
+
+// fully qualied package name
+/*Date date = new java.util.Date();*/     // gives error as full name used only at right
+
+java.util.Date date1 = new java.util.Date();
+}
+
+}
